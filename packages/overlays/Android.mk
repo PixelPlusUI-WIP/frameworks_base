57e3e325cdc92bca40c10f1d408f9501d3e79d9e
rezaadi0105 <rezaadipangestu5@gmail.com>
2020-03-22 21:40:04 +0700

    overlays: add Ancient6 icon shape
    Co-authored-by: kr15714n70 <kristiantoadiwibowo84@gmail.com>
    Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -115,6 +115,7 @@ LOCAL_REQUIRED_MODULES := \
 	IconShapeAncient3Overlay \
 	IconShapeAncient4Overlay \
         IconShapeAncient5Overlay \
+        IconShapeAncient6Overlay \
 	IconShapeRoundedRectOverlay \
 	IconShapeSquircleOverlay \
 	IconShapeTeardropOverlay \
