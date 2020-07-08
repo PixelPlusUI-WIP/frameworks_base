8e3c44e4faef0220f1177d0e935f6d6c9c323599
ugly-kid-af <dwarmachine24@gmail.com>
2020-07-08 14:36:37 +0000

    Revert "Add partial/full screenshot QS tile"
    This reverts commit d498fd3808caf22b18ba4a4ef4d04507152109cf.
    
    Signed-off-by: ugly-kid-af <dwarmachine24@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -48,7 +48,6 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenRecordTile;
-import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.SoundTile;
@@ -96,8 +95,6 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<SoundTile> mSoundTileProvider;
     private final Provider<SyncTile> mSyncTileProvider;
     private final Provider<VpnTile> mVpnTileProvider;
-    private final Provider<ScreenshotTile> mScreenshotTileProvider;
-
     private QSTileHost mHost;
 
     @Inject
@@ -129,8 +126,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<GamingModeTile> gamingModeTileProvider,
             Provider<SoundTile> soundTileProvider,
             Provider<SyncTile> syncTileProvider,
-            Provider<VpnTile> vpnTileProvider,
-            Provider<ScreenshotTile> screenshotTileProvider) {
+            Provider<VpnTile> vpnTileProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -160,7 +156,6 @@ public class QSFactoryImpl implements QSFactory {
         mSoundTileProvider = soundTileProvider;
         mSyncTileProvider = syncTileProvider;
         mVpnTileProvider = vpnTileProvider;
-        mScreenshotTileProvider = screenshotTileProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -234,8 +229,6 @@ public class QSFactoryImpl implements QSFactory {
                 return mSyncTileProvider.get();
             case "vpn":
                 return mVpnTileProvider.get();
-	    case "screenshot":
-                return mScreenshotTileProvider.get();
         }
 
         // Intent tiles.
